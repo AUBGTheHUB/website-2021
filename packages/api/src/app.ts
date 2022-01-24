bde872c6625fd944360abea76c075113cb0c5929
Ismayil Mirzali <ismayilmirzeli@gmail.com>
2022-01-24 20:38:09 +0200

    Setup MongoDB connection /w dotenv
    Signed-off-by: Ismayil Mirzali <ismayilmirzeli@gmail.com>

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,11 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connect } from 'mongoose';
 
 import routes from './routes';
+
+dotenv.config({ path: `${__dirname}/.env` });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(routes);
+
+if (!process.env.MONGO_URI) {
+  console.log('.env not loaded');
+  process.exit(1);
+}
+
+connect(process.env.MONGO_URI)
+  .then(() => console.log('Successfully established connection to database...'))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server is now running on ${PORT}...`);
