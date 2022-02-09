76e9edaa1ad2f1f3024a9593c2b63db57a2b6b62
mihailbozhilovjr@gmail.com <mihailbozhilov@gmail.com>
2022-02-09 22:34:09 +0200

    Add tsParticles and container animations

--- packages/web/src/components/HomeComponents/home.tsx
+++ packages/web/src/components/HomeComponents/home.tsx
@@ -4,10 +4,35 @@ import Navigation from './navbar';
 import './home.css';
 import TextBody from './homeTextBody';
 import Hubzie from './hubzie-the-hub.png';
+import Particles from 'react-tsparticles';
+import { useMediaQuery } from 'react-responsive';
 
 const Home = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 1000px)' });
+  const particlesSize = isMobile ? 20 : 55;
   return (
     <div className="background_image">
+      <Particles
+        params={{
+          fpsLimit: 60,
+          particles: {
+            color: {
+              value: '#000'
+            },
+            number: {
+              value: particlesSize
+            },
+            links: {
+              enable: true,
+              color: '#000',
+              distance: 150
+            },
+            move: {
+              enable: true
+            }
+          }
+        }}
+      />
       <Navigation></Navigation>
       <TextBody></TextBody>
       <Image src={Hubzie} className="hubzie"></Image>
