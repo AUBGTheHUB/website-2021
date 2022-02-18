1e27ed75d1fc2cbdc5df80d6fc516b532aea05ab
mihailbozhilov <mihailbozhilov@gmail.com name = Mihail Bozhilov>
2022-02-18 22:27:44 +0200

    Initializing HackAUBG Component

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/HomeComponents/home';
+import HackAUBG from './components/HomeComponents/hackaubg_landing/hackaubg';
 
 function App() {
-  return <Home></Home>;
+  return <HackAUBG></HackAUBG>;
 }
 
 export default App;
