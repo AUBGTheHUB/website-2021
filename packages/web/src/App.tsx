9eb80d6d4210e7d46dfd0463ca6f97f52fb33797
mihailbozhilov <mihailbozhilov@gmail.com name = Mihail Bozhilov>
2022-02-21 13:42:30 +0200

    Added Team Gallery component and Schedule Table

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/HomeComponents/home';
 import HackAUBG from './components/HomeComponents/hackaubg_landing/hackaubg';
+import MarketingTeam from './components/HomeComponents/meetTheTeam/marketingTeam';
 
 function App() {
   return <HackAUBG></HackAUBG>;
---
+++ packages/web/src/components/HomeComponents/meetTheTeam/logisticsTeam.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import './marketingTeam.css';
+
+const LogisticsTeam = () => {
+  return (
+    <div className="initialHolder">
+      <h1 className="headerText">Logistics Team</h1>
+      <div className="holder">
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img variant="top" src={process.env.PUBLIC_URL + 'krisi.png'} className="imageTag" />
+          <Card.Body>
+            <Card.Title>Krisi</Card.Title>
+            <Card.Text>
+              Our beloved president of THE HUB and miracle maker .. random words , fuck yeah , let's
+              go to Under!!!!
+            </Card.Text>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/kristina-borisova-447293142/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default LogisticsTeam;
