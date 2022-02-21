2fa2c50decb6bc807db83e14f08953ba4109d9d7
Nikolay Ninov <nikininov1@gmail.com>
2022-02-21 12:51:01 +0200

    adding email checking functionality to the signup controller

---
+++ packages/api/v1/signup/signUp.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { createSignUp } from '../../src/controllers/signUpController';
+
+export const signUpRoutes = Router();
+
+signUpRoutes.post('/api/v1/signup', createSignUp);
