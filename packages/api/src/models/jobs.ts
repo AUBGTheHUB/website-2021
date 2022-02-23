7e0bf5254750b922f6e531f247257e6aad244a7f
peti-krl <petia.dimkina@outlook.com>
2022-02-23 19:34:56 +0200

    job event model

---
+++ packages/api/src/models/jobs.ts
@@ -0,0 +1,24 @@
+import mongoose, {Schema, Document } from "mongoose";
+
+export interface JobInterface extends Document{
+  title: string;
+  companyName: string;
+  description: string;
+  location: string;
+  link: string;
+  date: Date;
+  socialMedia: string[];
+}
+
+const JobSchema: Schema = new Schema ({
+  title: {type: String, required: true},
+  companyName: {type: String, required: true},
+  description: {type: String, required: true},
+  location: {type: String, required: true},
+  link: {type: String, required: true},
+  date: {type: Date, required: true},
+  socialMedia: {type: [String], required: false}
+});
+
+const Event = mongoose.model<JobInterface>("Job", JobSchema);
+export default Event;
