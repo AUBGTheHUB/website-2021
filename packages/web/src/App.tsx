2327591e54e431349f0d832086b46ca258a5e17a
Misho <michaelbozhilov@gmail.com>
2022-02-24 15:32:21 +0200

    Added routing and removed sliding Team container

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -4,9 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/HomeComponents/home';
 import HackAUBG from './components/HomeComponents/hackaubg_landing/hackaubg';
 import MarketingTeam from './components/HomeComponents/meetTheTeam/marketingTeam';
-
+import PRTeam from './components/HomeComponents/meetTheTeam/prTeam';
+import DevelopmentTeam from './components/HomeComponents/meetTheTeam/developmentTeam';
+import LogisticsTeam from './components/HomeComponents/meetTheTeam/logisticsTeam';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
-  return <HackAUBG></HackAUBG>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="hackaubg" element={<HackAUBG></HackAUBG>}></Route>
+        <Route path="publicrelationsdepartment" element={<PRTeam></PRTeam>}></Route>
+        <Route path="marketingdepartment" element={<MarketingTeam></MarketingTeam>}></Route>
+        <Route path="developmentdepartment" element={<DevelopmentTeam></DevelopmentTeam>}></Route>
+        <Route path="logisticsdepartment" element={<LogisticsTeam></LogisticsTeam>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
