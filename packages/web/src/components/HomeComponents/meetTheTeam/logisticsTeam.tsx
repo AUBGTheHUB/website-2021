6fe131c189c2d0c60a5fdc34cb0ba898366d69c9
MiroGorksi <mijjo12@abv.bg>
2022-02-28 19:40:10 +0200

    Finished Team Gallery component

--- packages/web/src/components/HomeComponents/meetTheTeam/logisticsTeam.tsx
+++ packages/web/src/components/HomeComponents/meetTheTeam/logisticsTeam.tsx
@@ -8,13 +8,98 @@ const LogisticsTeam = () => {
       <h1 className="headerText">Logistics Team</h1>
       <div className="holder">
         <Card style={{ width: '18rem' }} className="cards">
-          <Card.Img variant="top" src={process.env.PUBLIC_URL + 'krisi.png'} className="imageTag" />
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Kaloyan.png'}
+            className="imageTag"
+          />
           <Card.Body>
-            <Card.Title>Krisi</Card.Title>
-            <Card.Text>
-              Our beloved president of THE HUB and miracle maker .. random words , fuck yeah , let's
-              go to Under!!!!
-            </Card.Text>
+            <Card.Title>Kaloyan - Head</Card.Title>
+
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/kristina-borisova-447293142/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img variant="top" src={process.env.PUBLIC_URL + 'Gabi.png'} className="imageTag" />
+          <Card.Body>
+            <Card.Title>Gabriela</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/kristina-borisova-447293142/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img variant="top" src={process.env.PUBLIC_URL + 'Mira.png'} className="imageTag" />
+          <Card.Body>
+            <Card.Title>Mira</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/kristina-borisova-447293142/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img variant="top" src={process.env.PUBLIC_URL + 'Sevo.png'} className="imageTag" />
+          <Card.Body>
+            <Card.Title>Svetoslav</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/kristina-borisova-447293142/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Zdravko.png'}
+            className="imageTag"
+          />
+          <Card.Body>
+            <Card.Title>Zdravko</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/kristina-borisova-447293142/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Teodora.png'}
+            className="imageTag"
+          />
+          <Card.Body>
+            <Card.Title>Teodora</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/teodora-miteva-34112a223/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Plamen.png'}
+            className="imageTag"
+          />
+          <Card.Body>
+            <Card.Title>Plamen</Card.Title>
             <Button
               className="socialButton"
               href="https://www.linkedin.com/in/kristina-borisova-447293142/"
--- packages/web/src/components/HomeComponents/meetTheTeam/prTeam.tsx
+++ packages/web/src/components/HomeComponents/meetTheTeam/prTeam.tsx
@@ -8,13 +8,111 @@ const PRTeam = () => {
       <h1 className="headerText">Public Relations Team</h1>
       <div className="holder">
         <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img variant="top" src={process.env.PUBLIC_URL + 'Marie.png'} className="imageTag" />
+          <Card.Body>
+            <Card.Title>Marie - Head</Card.Title>
+
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/marie-goygadjieva-a280321b0/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
           <Card.Img variant="top" src={process.env.PUBLIC_URL + 'krisi.png'} className="imageTag" />
           <Card.Body>
             <Card.Title>Krisi</Card.Title>
-            <Card.Text>
-              Our beloved president of THE HUB and miracle maker .. random words , fuck yeah , let's
-              go to Under!!!!
-            </Card.Text>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/kristina-borisova-447293142/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Ivan Asen.png'}
+            className="imageTag"
+          />
+          <Card.Body>
+            <Card.Title>Ivan-Asen</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/ivan-enchev-79a5a91aa/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img variant="top" src={process.env.PUBLIC_URL + 'Diana.png'} className="imageTag" />
+          <Card.Body>
+            <Card.Title>Diana</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/diana-materova-429595201/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Boyanna.png'}
+            className="imageTag"
+          />
+          <Card.Body>
+            <Card.Title>Boyana</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/boyana-chopeva-5866011b0/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Christiyana.png'}
+            className="imageTag"
+          />
+          <Card.Body>
+            <Card.Title>Kristiyana</Card.Title>
+            <Button
+              className="socialButton"
+              href="https://www.linkedin.com/in/kristiyana-gavrailova-605590212/"
+            >
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Mariela.png'}
+            className="imageTag"
+          />
+          <Card.Body>
+            <Card.Title>Mariela</Card.Title>
+            <Button className="socialButton" href="https://www.linkedin.com/in/mariela-nikolova/">
+              Find on LinkedIn
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }} className="cards">
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + 'Tihomir.png'}
+            className="imageTag"
+          />
+          <Card.Body>
+            <Card.Title>Tihomir</Card.Title>
             <Button
               className="socialButton"
               href="https://www.linkedin.com/in/kristina-borisova-447293142/"
