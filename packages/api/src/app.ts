c4a86e874ad17206c90c4f4b7a0449727c9d0a71
Nikolay Ninov <nikininov1@gmail.com>
2022-03-03 22:24:41 +0200

    fixing the signup logic

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { connect } from 'mongoose';
 import { eventRoutes } from '../v1/events/events';
@@ -9,6 +10,7 @@ dotenv.config({ path: `${__dirname}/.env` });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(eventRoutes);
 app.use(signUpRoutes);
